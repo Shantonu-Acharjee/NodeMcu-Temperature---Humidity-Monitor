21acb8d30320a444855cd11beca339cabb3b1c55
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-03-10 22:42:40 +0600

    Done

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,9 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <ThingSpeak.h>
+
+DHT dht(D5, DHT11);
+
+WiFiClient client;
+
+long myChannelNumber = 1672046;
+const char myWriteAPIKey[] = "TWLWM1JK8RJJPQ4M";
+
+
+
+float temp;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  WiFi.begin("Azad Sir Mess", "12345678");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print("..");
+  }
+  Serial.println();
+  Serial.println("NodeMCU is connected!");
+  Serial.println(WiFi.localIP());
+  dht.begin();
+  ThingSpeak.begin(client);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  //Serial.println("Temperature: " + (String) t);
+  //Serial.println("Humidity: " + (String) h);
+  //ThingSpeak.writeField(myChannelNumber, 1, t, myWriteAPIKey);
+  //ThingSpeak.writeField(myChannelNumber, 2, h, myWriteAPIKey);
+  delay(1000);
 
+
+
+  temp = analogRead(A0);
+  temp = temp / 3.1;
+  Serial.print("Temprateare : ");
+  Serial.print(temp);
+  Serial.print("*C");
+  Serial.println();
+  ThingSpeak.writeField(myChannelNumber, 1, temp, myWriteAPIKey);
 }
